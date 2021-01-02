7ae636075a0e4f3da35aa250c3a2b1061b593c92
Folley-05 <aristotepascaldjounda@gmail.com>
2021-01-02 20:20:32 +0100

    fin du module de recherche

--- src/component/layout/Navbar/Search.tsx
+++ src/component/layout/Navbar/Search.tsx
@@ -5,35 +5,34 @@ const Search: React.FC=(props)=>{
     const [text, setText]=useState('')
     const local=useContext(StorageContext)
     var Products=local.getAllProducts().map((data: any)=>({name: data.description, id: data.id}))
+    const [sug, setSug]=useState(new Array)
+    //var sug=new Array()
     const search=(e: any)=>{
-        setText(e.target.value)
+        //setText(e.target.value)
         check(e.target.value)
     }
     const check=(text: string)=>{
         var reg=new RegExp('^'+text, 'i');
-        var sug=[];
+        var suggestion=new Array()
         if(text){
             for (var i=0; i<Products.length; i++){
                 if(reg.test(Products[i].name)){
-                    var sugestion=document.createElement('div');
-                    sugestion=Products[i].name;
-                    sug.push(sugestion);
+                    suggestion.push(Products[i].name)
+                    //sug.push(Products[i].name);
                 }
             }
         }
-        console.log(sug)
+        setSug(suggestion)
     }
-
+    console.log(sug)
     return (
             <div className="row">
                 <div className="col-10 no__padding  search-bar">
-                    <input type="text" className='form-control search__input' typeof="search" value={text} onChange={e=>search(e)} />
+                    <input type="text" className='form-control search__input' typeof="search"  onChange={e=>search(e)} />
                     <div className="search-item-container">
-                        <div className="seach-item">item</div>
-                        <div className="seach-item">item</div>
-                        <div className="seach-item">item</div>
-                        <div className="seach-item">item</div>
-                        <div className="seach-item">item</div>
+                    {
+                        sug.map(item=><div className="seach-item">{item}</div>)
+                    }
                     </div>
                 </div>
                 <div className="col-2 no__padding">
