f25785d7251076b7d619816825e2908a153d60c1
HeAhCamerounToiAussiQuelleImage <beignetharicot@gmail.com>
2021-02-01 12:39:47 +0100

    Add image to panier

--- src/component/pages/Detail_produit/Detail_produit.tsx
+++ src/component/pages/Detail_produit/Detail_produit.tsx
@@ -8,13 +8,17 @@ import { act } from "react-dom/test-utils";
 
 import queryString from 'query-string';
 
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel/dist/assets/owl.theme.default.min.css';
+
 
 
 const Detail: React.FC = () => {
 
     const history = useHistory();
     const item = useSelector((state:any) => state.search.item)
-    const [productData, setProduct] = useState<any>([]);
+    const [productData, setProduct] = useState<any>();
     const [slider, setSlider] = useState<any>([]);
     const [isload, setLoader] = useState(false);
     const [quantity, setQuantity]=useState(1)
@@ -24,11 +28,9 @@ const Detail: React.FC = () => {
     const comand=useSelector((state: any)=> state.comand)
     const auth = useSelector((state:any) => state.auth)
 
-    console.log(comand)
+   
  
-    const toArray = (data: any) => {
-        return JSON.parse(data)
-    }
+  
 
 
     const process= async (parsed:any)=>{
@@ -95,46 +97,52 @@ const Detail: React.FC = () => {
 
         setLoader(true);
     }, [])
+
+    if (productData) 
+        JSON.parse(productData.images).map(e => {console.log(Api.imageUrl+e)})
     
-    return (
+    return ( 
         <div>
-            <div className="container">
+            <div className="container" style={{'display' : (productData && productData.images) ? 'block': 'none'}}>
                 <div className="row">
                     <div className="col-md-6 left__menu">
                         <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
-                            <div className="carousel-inner">
-                                { 
-                                    (productData.images) ?
-                                    (
-                                        toArray(productData.images).map((result: any) => (
-                                            <div className="carousel-item active" data-interval="1000">
-                                                <img src={Api.imageUrl + result} className="d-block w-100" />
-                                            </div>
-                                            )
-                                        )
-                                    ) : <div></div>
-                                }
-                                {/* {toArray(productData['images'])} */}
-                            </div>
-                            <a className="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="sr-only">Previous</span>
-                            </a>
-                            <a className="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="sr-only">Next</span>
-                            </a>
+                            
+                            <OwlCarousel
+                                            className="owl-theme"
+                                            items={1}
+                                            dots={true}
+                                            dotClass="dot-div"
+                                            dotsSpeed={true}
+                                            autoplay={true}
+                                            autoplaySpeed={500}
+                                            loop={true}
+                                        >
+
+                                        {
+                                            (productData ) ?
+                                                JSON.parse(productData.images).map(e => 
+                                                    <div className="item">
+                                                        <div className="slider-content">
+                                                            <img src={Api.imageUrl+e} className="slider__img_detail" alt="" />
+                                                        </div>
+                                                    </div>
+                                                ): <></>
+                                        }
+                                            
+                                           
+                                        </OwlCarousel>
                         </div>
                     </div>
                     <div className="col-md-6">
                         <div className="card mb-3">
                             <div className="card-body">
-                                <h5 className="card-title">{productData["titre"]}</h5>
+                                <h5 className="card-title">{(productData) ? productData.titre : ''}</h5>
                                 <p className="card-text">
-                                    {productData["description"]}
+                                    {(productData)? productData.description : ''}
                                 </p>
                                 <div className="alert alert-primary alert-link" role="alert">
-                                    {productData["prix"]} CFA
+                                    {(productData) ? productData.prix : ''} CFA
                             </div>
                                 <ul className="list-group list-group-flush">
                                     Quantité:
@@ -165,12 +173,12 @@ const Detail: React.FC = () => {
                         <u><p className="card-title description">Description du produit</p></u>
                         <div className="card-text">
                             <ul className="row">
-                                <li className="col-md-6 no-padding">Categorie : {productData["libelle_categorie"]} </li>
-                                <li className="col-md-6 no-padding">Ville : {productData["ville"]} </li>
-                                <li className="col-md-6 no-padding">Sexe : {productData["sexe"]} </li>
-                                <li className="col-md-6 no-padding">Telephone : {productData["telephone"]} </li>
-                                <li className="col-md-6 no-padding">Age : {productData["age"]} </li>
-                                <li className="col-md-6 no-padding">Lieu Boutique : {productData["lieu_boutique"]} </li>
+                                <li className="col-md-6 no-padding">Categorie : {(productData)?productData.libelle_categorie: ''} </li>
+                                <li className="col-md-6 no-padding">Ville : {(productData) ? productData.ville : ''} </li>
+                                <li className="col-md-6 no-padding">Sexe : {(productData) ? productData.sexe : ''} </li>
+                                <li className="col-md-6 no-padding">Telephone : {(productData) ? productData.telephone : ''} </li>
+                                <li className="col-md-6 no-padding">Age : {(productData)? productData.age: ''} </li>
+                                <li className="col-md-6 no-padding">Lieu Boutique : {(productData) ? productData.lieu_boutique : ''} </li>
                             </ul>
                             <div className="row justify-content-center">
                                 <button className="col-3 btn btn-success">Livraison</button>
